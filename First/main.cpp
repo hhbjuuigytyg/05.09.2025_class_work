6ad63d96cfca3ec1fe75a57e3db31573eae5efd9
Kono_pg11 <Kono_pg11@29-1>
2025-09-05 09:50:51 +0300

    fgfg

--- First/main.cpp
+++ First/main.cpp
@@ -2,7 +2,38 @@ using namespace std;
 
 #include <iostream>;
 
+class Pingas
+{
+private:
+	char* name = nullptr;
+	int x;
+	int y;
+
+public:
+	Pingas()
+	{
+
+	}
+
+	void Init(const char *nm)
+	{
+		if (name != nullptr)
+		{
+			delete[] name;
+		}
+		name = new char[strlen(nm) + 1];
+		strcpy_s(name, strlen(nm) + 1, nm); 
+	}
+
+	void Print()
+	{
+		cout << name;
+	}
+};
+
 int main()
 {
-
+	Pingas obj1;
+	obj1.Init("Fat");
+	obj1.Print();
 }
